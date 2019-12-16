43e81f5b13e297a75e762572404976877037d13f
Tyler Thompson <tthompson082@gmail.com>
2019-12-16 18:14:14 -0500

    Move FirebaseAuthUI to Login Component and Create Login Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
+
+import Login from './components/login.js';
+
 import './App.css';
 
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
@@ -13,48 +15,11 @@ const config = {
 firebase.initializeApp(config);
 
 function App() {
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
-  const uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    // We will display Google and Facebook as auth providers.
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccess: () => false
-    }
-  };
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      setIsSignedIn(!!user);
-      console.log('user', user);
-    });
-  }, []);
-
   return (
     <div className='App'>
       <h1>Flashcards</h1>
-
-      {isSignedIn ? (
-        <span>
-          <div>Signed In!</div>
-          <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-          <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-        </span>
-      ) : (
-        <StyledFirebaseAuth
-          uiConfig={uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
-      )}
-
       {/* //Links here */}
+      <Route path='/login' component={Login} />
 
       {/* //Switch Here */}
     </div>
