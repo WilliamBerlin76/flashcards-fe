aad98e997eff689c37e0e75d8435354b35602c2b
Tyler Thompson <tthompson082@gmail.com>
2019-12-16 18:01:31 -0500

    Create login.js component and add Imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,10 +46,6 @@ function App() {
           <div>Signed In!</div>
           <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
           <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-          <img
-            alt='profile picture'
-            src={firebase.auth().currentUser.photoURL}
-          />
         </span>
       ) : (
         <StyledFirebaseAuth
---
+++ frontend/src/components/login.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
