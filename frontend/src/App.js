09461341485df7fb34508c0e30340a56aca2e075
Kennethaustin <kenneth.austin93@gmail.com>
2020-01-06 12:45:31 -0500

    login

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Navigation from '../src/components/navigation';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flashcards</h1>
+
+      {/* //Links here */}
+
+
+
+
+      {/* //Switch Here */}
+    </div>
+  );
+}
+
+export default App;
