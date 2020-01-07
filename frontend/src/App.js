6d83641612a102d3c2800e377c781ff53aa0abfb
Kennethaustin <kenneth.austin93@gmail.com>
2020-01-07 14:47:51 -0500

    hamburger menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 import Navigation from '../src/components/navigation';
-// import
+import React from 'react';
+import { Burger, Menu } from './components';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import FocusLock from 'react-focus-lock';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef();
+  const menuId = "main-menu";
+
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div ref={node}>
+          <FocusLock disabled={!open}>
+            <Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+            <Menu open={open} setOpen={setOpen} id={menuId} />
+          </FocusLock>
+        </div>
     <div className="App">
       <h1>Flashcards</h1>
 
@@ -16,7 +33,8 @@ function App() {
 
       {/* //Switch Here */}
     </div>
+    </>
+    </ThemeProvider>
   );
 }
-
 export default App;
