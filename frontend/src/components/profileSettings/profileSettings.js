806a8d39230f55c142a24508ed695a0e7395cd86
Tyler Thompson <tthompson082@gmail.com>
2020-01-08 16:46:54 -0500

    Login redirects to dashboard and settings displays logged in user info
    Co-authored-by: WilliamBerlin76 <william.berlin76@gmail.com>

--- frontend/src/components/profileSettings/profileSettings.js
+++ frontend/src/components/profileSettings/profileSettings.js
@@ -1,152 +1,126 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
 import DashNav from '../dashNav/dashNav';
-import './profileSettings.scss'
+
+import './profileSettings.scss';
 const Settings = props => {
-    const [preferences, setPreferences] = useState({});
+  const [preferences, setPreferences] = useState({});
 
-    const nonCheckChange = e => {
-        setPreferences({
-            ...preferences,
-            [e.target.name]: e.target.value
-        });
-        console.log(preferences)
-    };
+  useEffect(() => {
+    console.log(firebase.auth().currentUser.photoURL);
+  });
 
-    const radioChange = e => {
-        setPreferences({
-            ...preferences,
-            [e.target.name]: e.target.value
-        })
-        console.log(preferences)
-    };
+  const nonCheckChange = e => {
+    setPreferences({
+      ...preferences,
+      [e.target.name]: e.target.value
+    });
+    console.log(preferences);
+  };
 
-    const submitForm = e => {
-        e.preventDefault();
-        console.log(preferences)
-    }
+  const radioChange = e => {
+    setPreferences({
+      ...preferences,
+      [e.target.name]: e.target.value
+    });
+    console.log(preferences);
+  };
 
-    return(
-        <>
-        <div className="settings-header-container">
-            <DashNav />
-            <h2>name here</h2>
-            <img url="picture" alt="profile pic"/>
-        </div>
-        <form className='profile-form'>
-            <p>Which subjects do you use flashcards for most often?</p>
-                <input 
-                    className='subject-input'
-                    type='text'
-                    name='favSubjects'
-                    value={preferences.subjects}
-                    onChange={nonCheckChange}    
-                />
+  const submitForm = e => {
+    e.preventDefault();
+    console.log(preferences);
+  };
 
-            <p>Do you prefer studying on a mobile or desktop device?</p>    
-                <input
-                    type='radio'
-                    name='MobileOrDesktop'
-                    value='Mobile'
-                    onChange={radioChange}
-                />
-                    <span>Mobile</span>
-                <input
-                    type='radio'
-                    name='MobileOrDesktop'
-                    value='Desktop'
-                    onChange={radioChange}
-                />
-                    <span>Desktop</span>
+  return (
+    <>
+      <div className='settings-header-container'>
+        <DashNav />
+        <h2>{firebase.auth().currentUser.displayName}</h2>
+        <img src={firebase.auth().currentUser.photoURL} alt='profile pic' />
+      </div>
+      <form className='profile-form'>
+        <p>Which subjects do you use flashcards for most often?</p>
+        <input
+          className='subject-input'
+          type='text'
+          name='favSubjects'
+          value={preferences.subjects}
+          onChange={nonCheckChange}
+        />
 
-            <p>Do you prefer to study by</p>
-                <select name='technique'
-                        onChange={nonCheckChange}
-                >
-                <option hidden="">Please select one</option>
-                    <option
-                        value='Listening'
-                    >Listening</option>
-                    <option
-                        value='Doing'
-                    >Doing</option>
-                    <option
-                        value='Reading' 
-                    >Reading</option>
-                    <option
-                        value='Writing' 
-                    >Writing</option>
-                    <option
-                        value='Other' 
-                    >Other</option>
-                </select>
+        <p>Do you prefer studying on a mobile or desktop device?</p>
+        <input
+          type='radio'
+          name='MobileOrDesktop'
+          value='Mobile'
+          onChange={radioChange}
+        />
+        <span>Mobile</span>
+        <input
+          type='radio'
+          name='MobileOrDesktop'
+          value='Desktop'
+          onChange={radioChange}
+        />
+        <span>Desktop</span>
 
-            <p>How frequently do you want to study?</p>    
-                <select name='study-frequency'
-                        onChange={nonCheckChange}
-                >
-                    <option hidden="">Please select one</option>
-                    <option
-                        value='Once a day'
-                    >Once a day</option>
-                    <option
-                        value='Twice a Day'
-                    >Twice a day</option>
-                    <option
-                        value='Once a week' 
-                    >Once a week</option>
-                    <option
-                        value='Twice a week' 
-                    >Twice a week</option>
-                    <option
-                        value='Three times a week' 
-                    >Three times a week</option>
-                    <option
-                        value='Everyday' 
-                    >Everyday</option>
-                    <option
-                        value='Other' 
-                    >Other</option>
-                </select>
+        <p>Do you prefer to study by</p>
+        <select name='technique' onChange={nonCheckChange}>
+          <option hidden=''>Please select one</option>
+          <option value='Listening'>Listening</option>
+          <option value='Doing'>Doing</option>
+          <option value='Reading'>Reading</option>
+          <option value='Writing'>Writing</option>
+          <option value='Other'>Other</option>
+        </select>
 
-            <p>How often would you like to recieve notifications?</p>
-                <select name='notification-frequency'
-                        onChange={nonCheckChange}
-                >
-                    <option hidden="">Please select one</option>
-                    <option
-                        value="When I haven't met my goal in a day"
-                    >When I haven't met my goal in a day</option>
-                    <option
-                        value="When I haven't met my goal in a week"
-                    >When I haven't met my goal in a week</option>
-                    <option
-                        value='Everyday' 
-                    >Everyday</option>
-                    <option
-                        value="Don't send me notifications"
-                    >Don't send me notifications</option>
-                </select>
-            
-            <p>Do you prefer to study from decks that are</p>
-                <input 
-                    type='radio'
-                    name='customOrPremade'
-                    value='pre-made'
-                    onChange={radioChange}    
-                />
-                    <span>Pre-made</span>
-                <input
-                    type='radio'
-                    name='customOrPremade'
-                    value='custom'  
-                    onChange={radioChange}
-                />
-                    <span>Custom</span>
-        </form>
+        <p>How frequently do you want to study?</p>
+        <select name='study-frequency' onChange={nonCheckChange}>
+          <option hidden=''>Please select one</option>
+          <option value='Once a day'>Once a day</option>
+          <option value='Twice a Day'>Twice a day</option>
+          <option value='Once a week'>Once a week</option>
+          <option value='Twice a week'>Twice a week</option>
+          <option value='Three times a week'>Three times a week</option>
+          <option value='Everyday'>Everyday</option>
+          <option value='Other'>Other</option>
+        </select>
 
-        <button onClick={submitForm}>Save</button>
-        </>
-    )
-}
+        <p>How often would you like to recieve notifications?</p>
+        <select name='notification-frequency' onChange={nonCheckChange}>
+          <option hidden=''>Please select one</option>
+          <option value="When I haven't met my goal in a day">
+            When I haven't met my goal in a day
+          </option>
+          <option value="When I haven't met my goal in a week">
+            When I haven't met my goal in a week
+          </option>
+          <option value='Everyday'>Everyday</option>
+          <option value="Don't send me notifications">
+            Don't send me notifications
+          </option>
+        </select>
+
+        <p>Do you prefer to study from decks that are</p>
+        <input
+          type='radio'
+          name='customOrPremade'
+          value='pre-made'
+          onChange={radioChange}
+        />
+        <span>Pre-made</span>
+        <input
+          type='radio'
+          name='customOrPremade'
+          value='custom'
+          onChange={radioChange}
+        />
+        <span>Custom</span>
+      </form>
+
+      <button onClick={submitForm}>Save</button>
+    </>
+  );
+};
 
 export default Settings;
