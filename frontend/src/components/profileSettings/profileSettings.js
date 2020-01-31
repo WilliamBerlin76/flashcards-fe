d9ff368c0c9cfcbe59b1bb15ead93a42c4527e10
WilliamBerlin76 <william.berlin76@gmail.com>
2020-01-30 19:22:09 -0600

    basic desktop and mobile sylings set for preferences page

--- frontend/src/components/profileSettings/profileSettings.js
+++ frontend/src/components/profileSettings/profileSettings.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
 import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
-import { makeStyles } from '@material-ui/core/styles';
 import { orange } from '@material-ui/core/colors';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -29,23 +28,14 @@ const OrangeInput = withStyles({
     },
     '& .MuiInput-underline:after': {
       borderBottomColor: 'rgba(106, 92, 85, 0.5)'
-    }
+    } 
   }
 })(TextField);
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     '& .MuiTextField-root': {
-//       margin: theme.spacing(1),
-//       width: 5000,
-//     },
-//   },
-// }));
 
 const Settings = props => {
   const [preferences, setPreferences] = useState({});
   let history = useHistory();
-  // const classes = useStyles();
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(function(user) {
@@ -103,7 +93,7 @@ const Settings = props => {
             <h2 className='preferences-back' onClick={() => history.goBack()}>Preferences</h2>
           </div>
           <div className='selector-container'>
-            <span className='selector-text'>Demo deck</span>
+            {/* <span className='selector-text'>Demo deck</span> */}
             <span className='selector-text'>Log Out</span>
           </div>
         </div>
@@ -120,132 +110,134 @@ const Settings = props => {
 
       <form className='profile-form'>
         <h2 className='pref-h2'>User Preferences</h2>
-        {/* <p>Which subjects do you use flashcards for most often?</p> */}
-        <OrangeInput
-          className='subject-input'
-          label={preferences.favSubjects ? null : 'Subjects'}
-          type='text'
-          name='favSubjects'
-          helperText="List subjects you most frequently study"
-          value={!preferences.favSubjects ? null : preferences.favSubjects}
-          onChange={nonCheckChange}
-        />
+        <div className='input-sections'>
+          {/* <p>Which subjects do you use flashcards for most often?</p> */}
+          <OrangeInput
+            className='subject-input'
+            label={preferences.favSubjects ? null : 'Subjects'}
+            type='text'
+            name='favSubjects'
+            helperText="List subjects you most frequently study"
+            value={!preferences.favSubjects ? null : preferences.favSubjects}
+            onChange={nonCheckChange}
+          />
 
-        {/* <p>How frequently do you want to study?</p> */}
-        <TextField 
-          select 
-          name='studyFrequency' 
-          onChange={nonCheckChange}
-          SelectProps={{
-            native: true,
-          }}
-          className='subject-input'
-          helperText='How often would you like to study'
-        >
-          <option hidden='' value={preferences.studyFrequency}>{!preferences.studyFrequency ? 'Study frequency' : preferences.studyFrequency}</option>
-          <option value='Once a day'>Once a day</option>
-          <option value='Twice a Day'>Twice a day</option>
-          <option value='Once a week'>Once a week</option>
-          <option value='Twice a week'>Twice a week</option>
-          <option value='Three times a week'>Three times a week</option>
-          <option value='Everyday'>Everyday</option>
-          <option value='Other'>Other</option>
-        </TextField>
-        <p className='field-title'>Mobile or desktop preference</p>
-        <RadioGroup className='radio-container' name='MobileOrDesktop'>
-          <div>
-            <OrangeRadio
-              id='mobileId'
-              type='radio'
-              label='Mobile'
-              name='MobileOrDesktop'
-              value='Mobile'
-              onChange={radioChange}
-              checked={preferences.MobileOrDesktop === 'Mobile' ? true : false}
-            /><label className='rad-text' htmlFor='mobileId'>Mobile</label>
-            
-          </div>
-          <div>
-            <OrangeRadio
-              id='deskId'
-              type='radio'
-              name='MobileOrDesktop'
-              value='Desktop'
-              onChange={radioChange}
-              checked={preferences.MobileOrDesktop === 'Desktop' ? true : false}
-            />
-            <label className='rad-text' htmlFor='deskId'>Desktop</label>
-          </div>
-          <p className='helper-text'>Do you prefer to study on your mobile or desktop device</p>
-        </RadioGroup>
+          {/* <p>How frequently do you want to study?</p> */}
+          <TextField 
+            select 
+            name='studyFrequency' 
+            onChange={nonCheckChange}
+            SelectProps={{
+              native: true,
+            }}
+            className='subject-input'
+            helperText='How often would you like to study'
+          >
+            <option hidden='' value={preferences.studyFrequency}>{!preferences.studyFrequency ? 'Study frequency' : preferences.studyFrequency}</option>
+            <option value='Once a day'>Once a day</option>
+            <option value='Twice a Day'>Twice a day</option>
+            <option value='Once a week'>Once a week</option>
+            <option value='Twice a week'>Twice a week</option>
+            <option value='Three times a week'>Three times a week</option>
+            <option value='Everyday'>Everyday</option>
+            <option value='Other'>Other</option>
+          </TextField>
+          <p className='field-title'>Mobile or desktop preference</p>
+          <RadioGroup className='radio-container' name='MobileOrDesktop'>
+            <div>
+              <OrangeRadio
+                id='mobileId'
+                type='radio'
+                label='Mobile'
+                name='MobileOrDesktop'
+                value='Mobile'
+                onChange={radioChange}
+                checked={preferences.MobileOrDesktop === 'Mobile' ? true : false}
+              /><label className='rad-text' htmlFor='mobileId'>Mobile</label>
+              
+            </div>
+            <div>
+              <OrangeRadio
+                id='deskId'
+                type='radio'
+                name='MobileOrDesktop'
+                value='Desktop'
+                onChange={radioChange}
+                checked={preferences.MobileOrDesktop === 'Desktop' ? true : false}
+              />
+              <label className='rad-text' htmlFor='deskId'>Desktop</label>
+            </div>
+            <p className='helper-text'>Do you prefer to study on your mobile or desktop device</p>
+          </RadioGroup>
 
-        
-        <p className='field-title'>Do you prefer to study from decks that are</p>
-        <RadioGroup className='radio-container' name='customOrPremade'>
-          <div>
-            <OrangeRadio
-            id='preId'
-            type='radio'
-            name='customOrPremade'
-            value='pre-made'
-            onChange={radioChange}
-            checked={preferences.customOrPremade === "pre-made" ? true : false}
-            />
-            <label className='rad-text' htmlFor='preId'>Pre-made</label>
-          </div>
-        
-          <div>
-            <OrangeRadio
-              id='customId'
+          
+          <p className='field-title'>Do you prefer to study from decks that are</p>
+          <RadioGroup className='radio-container' name='customOrPremade'>
+            <div>
+              <OrangeRadio
+              id='preId'
               type='radio'
               name='customOrPremade'
-              value='custom'
+              value='pre-made'
               onChange={radioChange}
-              checked={preferences.customOrPremade === 'custom' ? true : false}
-            />
-            <label className='rad-text' htmlFor='customId'>Custom</label >
-          </div>
-          <p className='helper-text'>Do you prefer to study from pre-made or custom decks</p>
-        </RadioGroup>
-       
-        {/* <p>Do you prefer to study by</p>
-        <select name='technique' onChange={nonCheckChange} className='subject-input'>
-          <option hidden='' value={preferences.technique}>{!preferences.technique ? 'Please select one' : preferences.technique}</option>
-          <option value='Listening'>Listening</option>
-          <option value='Doing'>Doing</option>
-          <option value='Reading'>Reading</option>
-          <option value='Writing'>Writing</option>
-          <option value='Other'>Other</option>
-        </select> */}
-
+              checked={preferences.customOrPremade === "pre-made" ? true : false}
+              />
+              <label className='rad-text' htmlFor='preId'>Pre-made</label>
+            </div>
+          
+            <div>
+              <OrangeRadio
+                id='customId'
+                type='radio'
+                name='customOrPremade'
+                value='custom'
+                onChange={radioChange}
+                checked={preferences.customOrPremade === 'custom' ? true : false}
+              />
+              <label className='rad-text' htmlFor='customId'>Custom</label >
+            </div>
+            <p className='helper-text'>Do you prefer to study from pre-made or custom decks</p>
+          </RadioGroup>
         
+          {/* <p>Do you prefer to study by</p>
+          <select name='technique' onChange={nonCheckChange} className='subject-input'>
+            <option hidden='' value={preferences.technique}>{!preferences.technique ? 'Please select one' : preferences.technique}</option>
+            <option value='Listening'>Listening</option>
+            <option value='Doing'>Doing</option>
+            <option value='Reading'>Reading</option>
+            <option value='Writing'>Writing</option>
+            <option value='Other'>Other</option>
+          </select> */}
 
-        <TextField 
-          select
-           SelectProps={{
-            native: true,
-          }}
-          helperText="How often would you like to be notified"
-          name='notificationFrequency' 
-          onChange={nonCheckChange} 
-          className='subject-input'
-        >
-          <option hidden='' value={preferences.notificationFrequency}>{!preferences.notificationFrequency ? 'Notification frequency' : preferences.notificationFrequency}</option>
-          <option value="When I haven't met my goal in a day">
-            When I haven't met my goal in a day
-          </option>
-          <option value="When I haven't met my goal in a week">
-            When I haven't met my goal in a week
-          </option>
-          <option value='Everyday'>Everyday</option>
-          <option value="Don't send me notifications">
-            Don't send me notifications
-          </option>
-        </TextField>
+          
 
-        <div className='button-container'>
-          <button onClick={submitForm} className='bottom-button'>Save</button>
-        </div>
+          <TextField 
+            select
+            SelectProps={{
+              native: true,
+            }}
+            helperText="How often would you like to be notified"
+            name='notificationFrequency' 
+            onChange={nonCheckChange} 
+            className='subject-input'
+          >
+            <option hidden='' value={preferences.notificationFrequency}>{!preferences.notificationFrequency ? 'Notification frequency' : preferences.notificationFrequency}</option>
+            <option value="When I haven't met my goal in a day">
+              When I haven't met my goal in a day
+            </option>
+            <option value="When I haven't met my goal in a week">
+              When I haven't met my goal in a week
+            </option>
+            <option value='Everyday'>Everyday</option>
+            <option value="Don't send me notifications">
+              Don't send me notifications
+            </option>
+          </TextField>
+
+          <div className='button-container'>
+            <button onClick={submitForm} className='bottom-button'>Save</button>
+          </div>
+          </div>
       </form>
     </>
   );
