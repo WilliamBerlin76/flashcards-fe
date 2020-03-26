130df4b813b5a9dbfda15d7f4b4257f058e4f0b0
Gabriel Anguiano <anguianog97@gmail.com>
2020-03-26 09:01:22 -0700

    refactored chartjs to react-chartjs-2 w/ shane and cody

--- frontend/src/components/userMetrics/UserMetrics.js
+++ frontend/src/components/userMetrics/UserMetrics.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { bar, Line, Pie, Chart, doughnut, defaults } from 'chart.js';
+// import { bar, Line, Pie, Chart, doughnut, defaults } from 'chart.js';
+import { Doughnut, Bar, Line, Pie} from 'react-chartjs-2'
 import axios from "axios";
 import firebase from 'firebase'
 
 const UserMetrics = () => {
     const [myChart, setMyChart] = useState("bar");
+    const [newData, setNewData] = useState({ })
     const [userData, setUserData] = useState([]);
-    const [d, setDays] = useState({
+    const [days, setDays] = useState({
         Sun: {
             cardsCorrect: 0
         },
@@ -19,7 +21,7 @@ const UserMetrics = () => {
         Wed:  {
             cardsCorrect: 0
         },
-        Thurs:  {
+        Thu:  {
             cardsCorrect: 0
         },
         Fri:  {
@@ -30,14 +32,19 @@ const UserMetrics = () => {
         }
     })
 
-    console.log("res", userData);
+
+  
+
 
     useEffect(() => {
          firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
+                
+
                 // let id = firebase.auth().currentUser.uid;
                 let id = "4fIUocfjIbUFYs46CD0q9QtOdrO2"
                 const d = new Date().getDay();
+                
 
                 axios
                     .get(
@@ -46,19 +53,23 @@ const UserMetrics = () => {
                     .then(res => {
                         setUserData(res.data)
                         // console.log(res.data[0].date);
-                        // let date = res.data[0].date;
-                        // let newDate = ( date / (60*60*24*1000));
+                        //  let date = res.data[0].date;
+                        //  let newDate = ( date / (60*60*24*1000));
                         // console.log("newDate", date);
 
                         // 1585137600000
                         // console.log(newDate.toDateString());
-
+                      console.log(res.data)
+                       let itemDates = []
                         res.data.map(day => {
+                        
                             let newDate = new Date(day.date).toDateString();
-
+                              itemDates.push(newDate)
+                              console.log(itemDates)
                             let other = newDate.split(" ");
+                           
                             setDays({
-                                ...d,
+                                ...days,
                                 [other[0]]: day.metrics
                             })
                         })
@@ -73,56 +84,47 @@ const UserMetrics = () => {
         });
     }, [])
 
-    useEffect(() => {
-        // dailyStats(userData[0].date);
 
-        let statsChart = document.getElementById("metricChart").getContext("2d");
-        let chart = new Chart(statsChart, {
-            // if (chart != undefined)
-            type: myChart,
-            data: {
-                labels: ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"],
-                datasets: [{
-                    label: "THE dataset",
-                    data: [`${d.Sun.cardsCorrect}`, `${d.Mon.cardsCorrect}`, `${d.Tues.cardsCorrect}`, `${d.Wed.cardsCorrect}`, `${d.Thurs.cardsCorrect}`,, `${d.Fri.cardsCorrect}`, `${d.Sat.cardsCorrect}`],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                }],
-            }
-        })
-    }, [myChart])
+ useEffect(() =>{
+    setNewData({
+        data: {
+            labels:['Sun', 'Mon', 'Tue','Wed','Thur','Friday','Sat'],
+            datasets: [
+              {
+                label: "Cards Correct",
+                data: [ days.Sun.cardsCorrect, days.Mon.cardsCorrect, days.Tues.cardsCorrect, days.Wed.cardsCorrect, days.Thu.cardsCorrect, days.Fri.cardsCorrect, days.Sat.cardsCorrect],
+                backgroundColor: [
+                  "rgba(255, 99, 132, 1)",
+                  "rgba(54, 162, 235, 1)",
+                  "rgba(255, 206, 86, 1)",
+                  "rgba(153, 102, 255, 1)",
+                  "red",
+                  "orange",
+                  "gold",
+                ]
+              }
+            ]
+          }
+    })
+ }, [days])
 
-    const test = e => {
-        console.log(d);
-    }
+  
 
-    const typeSelect = (e) => {
-        e.preventDefault();
-        document.getElementById("someId").innerHTML = '&nbsp;';
-        document.getElementById("someId").innerHTML = '<canvas id="metricChart"></canvas>';
-        let val = e.target.value;
-        console.log(e.target.value)
-        setMyChart(val)
-    }
+
+   
 
     return (
         <div>
             <div className="chart-container">
-                <button onClick={test}>log</button>
-                <select id="chartType" onChange={typeSelect}>
+                
+                <select id="chartType">
                     <option value="bar">Bar</option>
                     <option value="line">Line</option>
                     <option value="pie">Pie</option>
                     <option value="doughnut">Doughnut</option>
                 </select>
 
-                <div id="someId"><canvas id="metricChart"></canvas></div>
+               <Bar data={newData.data} width={100} height={50}/>
             </div>
         </div>
     )
