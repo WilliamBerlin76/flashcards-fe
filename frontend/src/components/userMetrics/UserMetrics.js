9a100af047440a90bd87e49a7736760ff1dd9639
Shane Jeremich <shane.jeremich@gmail.com>
2020-03-25 18:55:46 -0600

    pushing to make changes later

--- frontend/src/components/userMetrics/UserMetrics.js
+++ frontend/src/components/userMetrics/UserMetrics.js
@@ -4,17 +4,36 @@ import axios from "axios";
 import firebase from 'firebase'
 
 const UserMetrics = () => {
-    const [potato, setPotato] = useState("bar");
-    const [userData, setUserData] = useState([])
+    const [myChart, setMyChart] = useState("bar");
+    const [userData, setUserData] = useState([]);
+    const [d, setDays] = useState({
+        Sun: {
+            cardsCorrect: 0
+        },
+        Mon:  {
+            cardsCorrect: 0
+        },
+        Tues:  {
+            cardsCorrect: 0
+        },
+        Wed:  {
+            cardsCorrect: 0
+        },
+        Thurs:  {
+            cardsCorrect: 0
+        },
+        Fri:  {
+            cardsCorrect: 0
+        },
+        Sat:  {
+            cardsCorrect: 0
+        }
+    })
 
-    console.log(userData);
-
-
+    console.log("res", userData);
 
     useEffect(() => {
-
-
-        firebase.auth().onAuthStateChanged(function (user) {
+         firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
                 // let id = firebase.auth().currentUser.uid;
                 let id = "4fIUocfjIbUFYs46CD0q9QtOdrO2"
@@ -26,6 +45,24 @@ const UserMetrics = () => {
                     )
                     .then(res => {
                         setUserData(res.data)
+                        // console.log(res.data[0].date);
+                        // let date = res.data[0].date;
+                        // let newDate = ( date / (60*60*24*1000));
+                        // console.log("newDate", date);
+
+                        // 1585137600000
+                        // console.log(newDate.toDateString());
+
+                        res.data.map(day => {
+                            let newDate = new Date(day.date).toDateString();
+
+                            let other = newDate.split(" ");
+                            setDays({
+                                ...d,
+                                [other[0]]: day.metrics
+                            })
+                        })
+
                     })
                     .catch(err => {
                         console.log(err);
@@ -34,16 +71,20 @@ const UserMetrics = () => {
                 return null;
             }
         });
+    }, [])
+
+    useEffect(() => {
+        // dailyStats(userData[0].date);
 
         let statsChart = document.getElementById("metricChart").getContext("2d");
         let chart = new Chart(statsChart, {
             // if (chart != undefined)
-            type: potato,
+            type: myChart,
             data: {
                 labels: ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"],
                 datasets: [{
                     label: "THE dataset",
-                    data: [3, 52, 59, 24, 98, 15, 75],
+                    data: [`${d.Sun.cardsCorrect}`, `${d.Mon.cardsCorrect}`, `${d.Tues.cardsCorrect}`, `${d.Wed.cardsCorrect}`, `${d.Thurs.cardsCorrect}`,, `${d.Fri.cardsCorrect}`, `${d.Sat.cardsCorrect}`],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -55,7 +96,11 @@ const UserMetrics = () => {
                 }],
             }
         })
-    }, [potato])
+    }, [myChart])
+
+    const test = e => {
+        console.log(d);
+    }
 
     const typeSelect = (e) => {
         e.preventDefault();
@@ -63,12 +108,13 @@ const UserMetrics = () => {
         document.getElementById("someId").innerHTML = '<canvas id="metricChart"></canvas>';
         let val = e.target.value;
         console.log(e.target.value)
-        setPotato(val)
+        setMyChart(val)
     }
 
     return (
         <div>
             <div className="chart-container">
+                <button onClick={test}>log</button>
                 <select id="chartType" onChange={typeSelect}>
                     <option value="bar">Bar</option>
                     <option value="line">Line</option>
