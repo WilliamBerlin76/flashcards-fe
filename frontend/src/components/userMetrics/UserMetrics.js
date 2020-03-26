cf7d3c9a6ace621c32a3f70bd1633bf74e0db09b
Gabriel Anguiano <anguianog97@gmail.com>
2020-03-26 13:25:05 -0700

    hard coded metric values w/ Shane and Cody

--- frontend/src/components/userMetrics/UserMetrics.js
+++ frontend/src/components/userMetrics/UserMetrics.js
@@ -1,14 +1,47 @@
-import React, { useState, useEffect } from "react";
-// import { bar, Line, Pie, Chart, doughnut, defaults } from 'chart.js';
+import React, { useState, useEffect, useRef } from "react";
 import { Doughnut, Bar, Line, Pie} from 'react-chartjs-2'
 import axios from "axios";
 import firebase from 'firebase'
 
+
+    const options = {
+        
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        
+    }
+
 const UserMetrics = () => {
-    const [myChart, setMyChart] = useState("bar");
-    const [newData, setNewData] = useState({})
+
+    const [newData, setNewData] = useState({
+        data: {
+            labels:['Sun', 'Mon', 'Tue','Wed','Thur','Friday','Sat'],
+            datasets: [
+            {
+                label: "Cards Correct",
+                data: [ 4, 5, 9, 5, 4, 10, 4],
+                backgroundColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(153, 102, 255, 1)",
+                "red",
+                "orange",
+                "gold",
+                ]
+            }
+            ],
+        }
+    });
+
+    const [myChart, setMyChart] = useState(<Bar data={newData.data} width={100} height={50} options={options}/>);
     const [userData, setUserData] = useState([]);
-    const [abr, setAbr] = useState([]);
+    const [thing, setThing] = useState(false);
     const [days, setDays] = useState({
         Sun: {
             cardsCorrect: 0
@@ -31,123 +64,104 @@ const UserMetrics = () => {
         Sat:  {
             cardsCorrect: 0
         }
-    })
+    });
 
+    // useEffect(() => {
+    //     firebase.auth().onAuthStateChanged ( async function (user)  {
+    //         if (user) {
+    //             // let id = firebase.auth().currentUser.uid;
+    //             let id = "4fIUocfjIbUFYs46CD0q9QtOdrO2"
+    //             const d = new Date().getDay();
 
-  let arr = [];
-
-
-    useEffect(() => {
-         firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                
-
-                // let id = firebase.auth().currentUser.uid;
-                let id = "4fIUocfjIbUFYs46CD0q9QtOdrO2"
-                const d = new Date().getDay();
-                
-
-                axios
-                    .get(
-                        `https://flashcards-be.herokuapp.com/api/metrics/${id}/${d}`
-                    )
-                    .then(res => {
-                        setUserData(res.data)
-                       
-                       console.log(res.data);
-                        // res.data.map(day => {
+    //             axios
+    //                 .get(
+    //                     `https://flashcards-be.herokuapp.com/api/metrics/${id}/${d}`
+    //                 )
+    //                 .then(res => {
+    //                     setUserData(res.data)
+    //                     for(let i = res.data.length -1; i >= 0; i--) {
+    //                         console.log(i)
+    //                         let newDate = new Date(res.data[i].date).toDateString();
+    //                         let other = newDate.split(" ");
                         
-                        //     let newDate = new Date(day.date).toDateString();
-                        //     console.log(newDate)
-                        //     let other = newDate.split(" ");
-                        //     console.log(other[0])
-                        //     setDays({
-                        //         ...days,
-                        //         [other[0]]: day.metrics
-                        //     })
-                        //     console.log(days)
-                        
+    //                         if(days.other[0].metrics.cardsCorrect === res.data.other[0].metrics.cardsCorrect) {
+    //                             return;
+    //                         } else {
+    //                             setDays({
+    //                                 ...days,
+    //                                 [other[0]]: res.data[i].metrics
+    //                             })
+    //                         }
+    //                     }
 
-                             
-                        //     //   setDays({
-                        //     //       ...days,
-                        //     //       [other[0]]: day.metrics
-                        //     //   })
-                        //     // daySet(other[0], day.metrics);
-                        // })
+    //                     setNewData({
+    //                         data: {
+    //                             labels:['Sun', 'Mon', 'Tue','Wed','Thur','Friday','Sat'],
+    //                             datasets: [
+    //                             {
+    //                                 label: "Cards Correct",
+    //                                 data: [ days.Sun.cardsCorrect, days.Mon.cardsCorrect, days.Tues.cardsCorrect, days.Wed.cardsCorrect, days.Thu.cardsCorrect, days.Fri.cardsCorrect, days.Sat.cardsCorrect],
+    //                                 backgroundColor: [
+    //                                 "rgba(255, 99, 132, 1)",
+    //                                 "rgba(54, 162, 235, 1)",
+    //                                 "rgba(255, 206, 86, 1)",
+    //                                 "rgba(153, 102, 255, 1)",
+    //                                 "red",
+    //                                 "orange",
+    //                                 "gold",
+    //                                 ]
+    //                             }
+    //                             ]
+    //                         }
+    //                     })
+    //                 })
+    //                 .catch(err => {
+    //                     console.log(err);
+    //                 });
+    //         } else {
+    //             return null;
+    //         }
+        
+    //     });
+    // }, [userData])   
 
-                        for(let i = res.data.length; i > 0; i--) {
-                            let newDate = new Date(res.data[i].date).toDateString();
-                            let other = newDate.split(" ");
-                            setDays({
-                                ...days,
-                                [other[0]]: res.data[i].metrics
-                            })
-                        }
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            } else {
-                return null;
-            }
-        });
-    }, [])
-
-    const daySet = () => {
-        setDays({
-            ...days,
-            [other[0]]: day.metrics
-        })
-    }  
-    useEffect(() => {
-    daySet()
-    },[days])
-
-
- useEffect(() =>{
-    setNewData({
-        data: {
-            labels:['Sun', 'Mon', 'Tue','Wed','Thur','Friday','Sat'],
-            datasets: [
-              {
-                label: "Cards Correct",
-                data: [ days.Sun.cardsCorrect, days.Mon.cardsCorrect, days.Tues.cardsCorrect, days.Wed.cardsCorrect, days.Thu.cardsCorrect, days.Fri.cardsCorrect, days.Sat.cardsCorrect],
-                backgroundColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "red",
-                  "orange",
-                  "gold",
-                ]
-              }
-            ]
-          }
-    })
- }, [days])
-
-  
-
-    const test = e => {
-        // console.log(abr);
-        console.log(days);
+    const chartSelect = e => {
+        console.log(e.target.value)
+        switch(e.target.value) {
+            case "Bar":
+                setMyChart(
+                    <Bar data={newData.data} width={100} height={50} options={options}/>
+                )
+                break;
+            case "Line":
+                setMyChart(
+                    <Line data={newData.data} width={100} height={50}  options={options}/>
+                )
+                break;
+            case "Doughnut":
+                setMyChart(
+                    <Doughnut data={newData.data} width={100} height={50}  options={options}/>
+                )
+                break;
+            case "Pie":
+                setMyChart(
+                    <Pie data={newData.data} width={100} height={50}  options={options}/>
+                )
+                break;
+        }
     }
-   
 
     return (
         <div>
             <div className="chart-container">
-                <button onClick={test}>Test</button>
-                <select id="chartType">
-                    <option value="bar">Bar</option>
-                    <option value="line">Line</option>
-                    <option value="pie">Pie</option>
-                    <option value="doughnut">Doughnut</option>
+                <select id="chartType" onChange={chartSelect}>
+                    <option value="Bar">Bar</option>
+                    <option value="Line">Line</option>
+                    <option value="Pie">Pie</option>
+                    <option value="Doughnut">Doughnut</option>
                 </select>
 
-               <Bar data={newData.data} width={100} height={50}/>
+               {myChart}
             </div>
         </div>
     )
